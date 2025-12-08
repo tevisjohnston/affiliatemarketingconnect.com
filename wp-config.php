3508a7794f706ea889aea2b2dc97bf27f34c050c
Tevis Johnston <tevisjohnston@gmail.com>
2025-12-07 22:39:00 -0800

    wp-config.php.

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'default' );
+define( 'DB_NAME', 'mysql-database-mkkso88ok8co84oww084s4gg' );
 
 /** Database username */
 define( 'DB_USER', 'mysql' );
 
 /** Database password */
-define( 'DB_PASSWORD', '6Nq3mUimfsg0banMQ9AsPkMO2XmdEOqBxp3LAwfKvBbzB9smHzMzOABloPKkUz0D' );
+define( 'DB_PASSWORD', 'gMZf8aDTHA7cQtDTMtmsRhoLrkCmya86q7VkYAOp7tXgNczfRYCLx4l1e8624raz' );
 
 /** Database hostname */
 define( 'DB_HOST', '72.60.231.242' );
